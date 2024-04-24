26fff2dadf363fd7af1c91dbec4cd936a4d6fc86
AnnDo33 <ann.dominique.d@gmail.com>
2024-04-24 12:33:55 +0200

    Ajout du composant image pokemon

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,12 @@
+function PokemonCard() {
+
+  return (
+    <figure className="pokemonCard">
+     <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Image du Pokémon" />
+      <figcaption>
+        <h1>Bulbasaur</h1>      
+      </figcaption>
+    </figure>
+  )
+}
+  export default PokemonCard;
